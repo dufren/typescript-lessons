f02a41a19fe7fc7763bfad27f5d060d1984a1bce
Özgür ÇOKÇEKEN <ozgur.cokceken@istinye.edu.tr>
2023-01-24 16:50:46 +0300

    lesson03

---
+++ lesson03/src/main.ts
@@ -0,0 +1,12 @@
+let stringArr = ["one", "hey", "three"]
+let guiatars = ["one", "hey", 5150]
+let mixedData = ["one", true, 5150]
+
+stringArr[0] = "eyyy"
+stringArr.push("levo")
+
+guiatars[0] = 1984
+guiatars.unshift("jim")
+
+let test = []
+let bands: string[] = []
