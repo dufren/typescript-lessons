fc299fbf71da4a578db82c282d291890b9fa9484
Özgür ÇOKÇEKEN <ozgur.cokceken@istinye.edu.tr>
2023-01-24 14:11:27 +0300

    lesson02

--- lesson02/build/js/main.js
+++ lesson02/build/js/main.js
@@ -1,3 +1,16 @@
 "use strict";
-let myName = "zgr";
-console.log(myName);
+let myName;
+let meaningOfLife;
+let isLoading;
+let album;
+let bigBro;
+myName = "zgr";
+meaningOfLife = 44;
+isLoading = true;
+album = false;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- lesson02/src/main.ts
+++ lesson02/src/main.ts
@@ -1,2 +1,19 @@
-let myName = "zgr"
-console.log(myName)
+let myName: string
+let meaningOfLife: number
+let isLoading: boolean
+let album: any
+let bigBro: string | number
+
+myName = "zgr"
+meaningOfLife = 44
+isLoading = true
+album = false
+
+const sum = (a: number, b: number) => {
+    return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean | string
+
+let re: RegExp = /\w+/g
