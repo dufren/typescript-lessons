e813c55b83ffcd15485c36d13fac9f337351b15e
Özgür ÇOKÇEKEN <ozgur.cokceken@istinye.edu.tr>
2023-01-25 13:30:55 +0300

    lesson03

--- lesson03/build/js/main.js
+++ lesson03/build/js/main.js
@@ -8,3 +8,46 @@ guiatars[0] = 1984;
 guiatars.unshift("jim");
 let test = [];
 let bands = [];
+bands.push("van halen");
+// tuples
+let myTuple = ["zgr", 42, false];
+let mixed = ["John", 1, false];
+myTuple[1] = 44;
+// objects
+let myObject;
+myObject = [];
+console.log(typeof myObject);
+myObject = bands;
+myObject = {};
+const exampleObject = {
+    prop1: "Dave",
+    prop2: false
+};
+exampleObject.prop1 = "zgR";
+let evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "ororo"]
+};
+let JP = {
+    name: "Kimmy",
+    albums: ["I", "II", "IV"]
+};
+evh = JP;
+const greetDeneme = (deneme) => {
+    if (deneme.name) {
+        return `hello ${deneme.name.toUpperCase()}!`;
+    }
+    return "eyyo!";
+};
+console.log(greetDeneme(evh));
+//enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 4] = "U";
+    Grade[Grade["D"] = 5] = "D";
+    Grade[Grade["C"] = 6] = "C";
+    Grade[Grade["B"] = 7] = "B";
+    Grade[Grade["A"] = 8] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- lesson03/src/main.ts
+++ lesson03/src/main.ts
@@ -10,3 +10,59 @@ guiatars.unshift("jim")
 
 let test = []
 let bands: string[] = []
+bands.push("van halen")
+
+// tuples
+let myTuple: [string, number, boolean] = ["zgr", 42, false]
+let mixed = ["John", 1, false]
+myTuple[1] = 44
+
+// objects
+let myObject: object
+myObject = []
+console.log(typeof myObject)
+myObject = bands
+myObject = {}
+
+const exampleObject = {
+    prop1: "Dave",
+    prop2: false
+}
+
+exampleObject.prop1 = "zgR"
+
+type Guitarist = { //interface de kullanılabilir
+    name?: string,
+    active?: boolean,
+    albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "ororo"]
+}
+
+let JP: Guitarist = {
+    name: "Kimmy",
+    albums: ["I", "II", "IV"]
+}
+evh = JP
+
+const greetDeneme = (deneme: Guitarist) => {
+    if (deneme.name) {
+        return `hello ${deneme.name.toUpperCase()}!`
+    }
+    return "eyyo!"
+}
+console.log(greetDeneme(evh))
+
+//enums
+enum Grade {
+    U = 4,
+    D,
+    C,
+    B,
+    A,
+}
+console.log(Grade.U)
